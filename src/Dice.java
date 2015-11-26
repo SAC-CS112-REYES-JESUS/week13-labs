31af30419c3421bb038a9530a1177f087327f620
a213 <a213@ISA2130029.sac.rsccd.inst>
2015-11-25 19:44:35 -0800

    1 cpmmit

---
+++ src/Dice.java
@@ -0,0 +1,15 @@
+
+public class Dice {
+
+	
+	private int RandomDice;
+	
+	public int Throw()
+	{
+		int random = (int) (Math.random()*6);
+		RandomDice = random;
+		return random;
+	}
+	
+	
+}
---
+++ src/ThreeDice.java
@@ -0,0 +1,27 @@
+
+public class ThreeDice {
+
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.print("hello\n");
+		
+		Dice Dice1 = new Dice();
+		Dice Dice2 = new Dice();
+		Dice Dice3 = new Dice();
+		int one=Dice1.Throw();
+		int two=Dice2.Throw();
+		int three=Dice3.Throw();
+		System.out.println(one);
+		System.out.println(two);
+		System.out.println(three);
+		
+		
+		int ave= ((one+two+three)/3);
+		System.out.printf("Average=%d",ave);
+		
+		
+		
+	}
+}
